d7dc54e0489aaad10accc3c238878858b265a8ff
shin-justin <j.a-smith@hotmail.com>
2024-11-16 23:44:48 +0000

    feat:(navbar): implement responsive navbar (WIP)

---
+++ responsive/navbar/index.js
@@ -0,0 +1,8 @@
+const hamburgerMenu = document.querySelector('.hamburger');
+const responsiveMenu = document.querySelector('.responsive-menu');
+
+function toggleMenu() {
+  //   responsiveMenu.style.display = 'flex';
+}
+
+hamburgerMenu.addEventListener('click', toggleMenu);
