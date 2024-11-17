9c9b3539180d29bddd1a99be36f9dc607c482165
shin-justin <j.a-smith@hotmail.com>
2024-11-17 17:04:10 +0000

    feat(navbar): implement responsive navbar full functionality

--- responsive/navbar/index.js
+++ responsive/navbar/index.js
@@ -1,8 +1,44 @@
-const hamburgerMenu = document.querySelector('.hamburger');
-const responsiveMenu = document.querySelector('.responsive-menu');
+const responsiveMenuApi = (() => {
+  const BREAKPOINT = 768;
+  let initListeners;
+  let responsiveMenu;
+  let isMenuOpen = false;
 
-function toggleMenu() {
-  //   responsiveMenu.style.display = 'flex';
-}
+  function hideMenu(menuEl) {
+    menuEl.style.display = 'none';
+    isMenuOpen = false;
+  }
 
-hamburgerMenu.addEventListener('click', toggleMenu);
+  function showMenu(menuEl) {
+    menuEl.style.display = 'flex';
+    isMenuOpen = true;
+  }
+
+  function toggleMenu() {
+    const elStyle = window.getComputedStyle(responsiveMenu);
+    if (elStyle.display === 'flex') {
+      hideMenu(responsiveMenu);
+    } else {
+      showMenu(responsiveMenu);
+    }
+  }
+
+  initListeners = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+      responsiveMenu = document.querySelector('.responsive-menu');
+      const hamburgerMenu = document.querySelector('.hamburger');
+      hamburgerMenu.addEventListener('click', toggleMenu);
+      window.addEventListener('resize', () => {
+        if (isMenuOpen && window.innerWidth > BREAKPOINT) {
+          hideMenu(responsiveMenu);
+        }
+      });
+    });
+  };
+
+  return {
+    initListeners
+  };
+})();
+
+responsiveMenuApi.initListeners();
